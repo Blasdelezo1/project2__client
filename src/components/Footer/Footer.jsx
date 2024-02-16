4cbdd8e0c2c5ac8a7939de305f3fd1c02429ce53
pedro roca de togores <pedrobgrdt@gmail.com>
2024-02-16 10:02:14 +0100

    all depencies

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import './Footer.css'
+
+
+
+const Footer = () => {
+
+    return (
+
+        <footer className='footer'>
+            <div className='footer-github'>
+
+            </div>
+            <div className='footer-copyright'>
+                <p>&copy; 2024 Pedro Roca de Togores y Maria Nogales </p>
+            </div>
+        </footer>
+
+    )
+
+}
+
+export default Footer
---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,25 @@
+import './NavBar.css'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+
+function NavBar() {
+
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">DREAMERS</Navbar.Brand>
+                    <Nav className="me-auto">
+
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
+
+
+
+}
+
+export default NavBar
