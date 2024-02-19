1d0c0f337df4ce444808fe84f37759c064917fd8
pedro roca de togores <pedrobgrdt@gmail.com>
2024-02-19 17:19:13 +0100

    create form done

--- src/pages/CreateFestivalPage.jsx
+++ src/pages/CreateFestivalPage.jsx
@@ -1,84 +1,19 @@
-import { Container, Button, Form, Row, Col } from "react-bootstrap"
-import { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import FormContactUs from "../components/FormContactUs/FormContactUs";
-import FormReviews from "../components/FormReviews/FormReviews";
+import { Container } from "react-bootstrap"
+import NewFestivalForm from "../components/NewFestivalForm/NewFestivalForm"
 
-// const API_BASE_URL = 'localhost:5005'
 
 const CreateFestivalPage = () => {
 
-    // const [festival, setFestival] = useState({
-    //     name: '',
-    //     description: ''
-
-    // })
-
-    // const navigate = useNavigate()
-
-    // const handleFormSubmit = (e) => {
-    //     e.preventDefeault()
-
-    //     axios
-    //     .post(`${}`)
-    // }
 
     return (
 
-        <div className="CreateFestivalPage">
+        <Container>
 
-            <Container>
-                <Row>
-                    <Col md={{ span: 6, offset: 2 }}>
-                        <h3>New Festival</h3>
-                        <hr />
-                        <Form className="mt-5">
-                            <Row>
-                                <Col>
-                                    <Form.Group className="mb-3" controlId="title">
-                                        <Form.Label>Fest Title </Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            value=''
-                                            onChange=''
-                                            name={'title'}
-                                        />
-                                        <Form.Text className="text-muted"></Form.Text>
-                                    </Form.Group>
-                                </Col>
+            <h1>Create Festival</h1>
 
-                                <Form.Group className="mb-3" controlId="description">
-                                    <Form.Label>Description</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        value=''
-                                        onChange=''
-                                        name={'description'}
-                                    />
-                                </Form.Group>
+            <NewFestivalForm />
 
-                                <Col>
-
-                                </Col>
-
-                            </Row>
-
-                        </Form>
-
-                    </Col>
-
-                </Row>
-
-            </Container>
-            <div>
-                <FormContactUs />
-            </div>
-            <FormReviews />
-
-        </div>
-
-
+        </Container >
     )
 
 }
