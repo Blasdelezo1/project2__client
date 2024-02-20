88d0e516519af10e9d2b5fb3b70d1bc2b5bae6a4
Maria Nogales <maria.nogales99@gmail.com>
2024-02-20 18:01:49 +0100

    Button back added to EditionPage and FestivalPage

--- src/components/TicketsCard/TicketsCard.jsx
+++ src/components/TicketsCard/TicketsCard.jsx
@@ -1,11 +1,13 @@
 import { Card } from "react-bootstrap"
 
+import './TicketsCard.css'
+
 const TicketsCard = ({ type, price }) => {
 
     return (
         <Card.Body key={price} className="TicketsCard">
-            <Card.Title>{type}</Card.Title>
-            <Card.Text>{price}</Card.Text>
+            <Card.Text className="type">Type: {type}</Card.Text>
+            <Card.Text className="priceTicketCard">Price: <strong>{price}€</strong></Card.Text>
         </Card.Body>
     )
 
--- src/pages/EditionDetailsPage/EditionDetailsPage.jsx
+++ src/pages/EditionDetailsPage/EditionDetailsPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
-import { Container, Spinner, Carousel, Card, CardBody, ListGroup, Figure, Nav } from "react-bootstrap"
+import { Container, Spinner, Carousel, Card, ListGroup, Row, Col, Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 import SponsorCard from './../../components/SponsorCard/SponsorCard'
 import TicketsCard from './../../components/TicketsCard/TicketsCard'
@@ -56,26 +57,27 @@ const EditionsDetailsPage = () => {
                         <h1>{festival.name} edition {edition.year}</h1>
                     </>
                 }
-
-                <Card className="editionInformation">
-                    <Card.Body>
-                        From {edition.starts} to {edition.ends}
-                    </Card.Body>
-                    <Card.Body>
-                        Capacity: {edition.capacity}
-                    </Card.Body>
-                    <Card.Body>
-                        Stages: {edition.stages}
-                    </Card.Body>
-                    <CardBody>
+                <Row>
+                    <Col md={5}>
+                        <Card.Body>
+                            <p>Relive the unforgettable moments of the <strong>{edition.year}</strong> edition of the <strong>{festival.name}</strong>!</p>
+                            <p>Start date: <strong>{edition.starts}</strong></p>
+                            <p>End date: <strong>{edition.ends}</strong></p>
+                            <p>We are pleased to reminisce and celebrate the incredible moments we shared during this unique experience. With a capacity for <strong>{edition.capacity}</strong> enthusiasts, we are grateful for the energy and passion that each of you brought to the festival.</p>
+                            <p>With <strong>{edition.stages}</strong> stages that came to life with music and art, this edition was an exciting journey through different genres and creative expressions. From intimate performances to dazzling spectacles, each moment was an opportunity to connect and celebrate diversity.</p>
+                            <p>As we look back, we fondly remember the shared moments, the laughter, the friendships forged, and the music that filled our hearts. Thank you for being part of this unforgettable experience!</p>
+                            <p>We hope to see you again in future editions of the <strong>{festival.name}</strong>!</p>
+                        </Card.Body>
+                    </Col>
+                    <Col md={5}>
                         <Card>
-                            <Card.Header>Tickets</Card.Header>
+                            <Card.Header> <strong>Tickets</strong> </Card.Header>
                             {
                                 edition.tickets.map((ticket, idx) => <TicketsCard {...ticket} key={idx} />)
                             }
                         </Card>
-                    </CardBody>
-                </Card>
+                    </Col>
+                </Row>
 
                 <ListGroup horizontal>
                     {
@@ -93,6 +95,13 @@ const EditionsDetailsPage = () => {
                         })
                     }
                 </Carousel>
+                <Row>
+                    <Col>
+                        <Link to={"/editions"}>
+                            <Button variant="dark">Back</Button>
+                        </Link>
+                    </Col>
+                </Row>
 
             </Container>
     )
