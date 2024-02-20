d603f8a32396512c9cfb5faca972d03c302d9771
pedro roca de togores <pedrobgrdt@gmail.com>
2024-02-20 13:38:55 +0100

    Hp almost done

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,38 +1,41 @@
-import { Carousel } from 'react-bootstrap'
+import { Carousel, Container } from 'react-bootstrap'
 import './HomePage.css'
+import burningManHp from './../../assets/images/burning-man.jpg'
+import tommorrrowLandHp from './../../assets/images/tommorrowland-hp-image.jpg'
+import snowBombingHp from './../../assets/images/snowbombing-HP-image.webp'
+import ultraEuropeHp from './../../assets/images/ultraeu_HP_image.jpeg'
+import primaveraSoundHp from './../../assets/images/primaverasound_HP_image.jpg'
 
 const HomePage = () => {
 
 
     return (
 
-        <>
-            <Carousel>
-                <Carousel.Item interval={1000}>
+        <div className='HomePage'>
+            <Container>
 
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
+                <Carousel>
+                    <Carousel.Item>
+                        <img src={burningManHp} />
+                    </ Carousel.Item>
+                    <Carousel.Item>
+                        <img src={tommorrrowLandHp} />
+                    </ Carousel.Item>
+                    <Carousel.Item>
+                        <img src={snowBombingHp} />
+                    </ Carousel.Item>
+                    <Carousel.Item>
+                        <img src={ultraEuropeHp} />
+                    </ Carousel.Item>
+                    <Carousel.Item>
+                        <img src={primaveraSoundHp} />
+                    </ Carousel.Item>
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
+                </Carousel>
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </>
+            </Container>
+
+        </div>
 
 
     )
