388c14693b459abc420f7d5f3787b2db8cc99c89
Maria Nogales <maria.nogales99@gmail.com>
2024-02-20 11:30:19 +0100

    CreateFestivalForm finished

--- src/components/NewFestivalForm/NewFestivalForm.jsx
+++ src/components/NewFestivalForm/NewFestivalForm.jsx
@@ -1,15 +1,31 @@
-import { Container, Button, Form, Row, Col } from "react-bootstrap"
 import { useState } from "react"
-import axios from "axios"
 import { useNavigate } from "react-router-dom"
+import { Container, Button, Form, Row, Col, InputGroup } from "react-bootstrap"
+
+import axios from "axios"
+import './NewFestivalForm.css'
 
 const API_BASE_URL = 'http://localhost:5005'
+
+const genres = ["Pop", "Rock", "Hip-hop", "R&B", "Jazz", "Blues", "Country", "Techno", "House", "Trance", "Drum and Bass", "Reggae", "Punk", "Metal", "Indie", "Alternative", "Funk", "Disco"]
 
 const NewFestivalForm = () => {
 
     const [newFestival, setNewFestival] = useState({
         name: '',
-        description: ''
+        shortDescription: '',
+        description: '',
+        location: {
+            country: '',
+            city: '',
+            zipcode: ''
+        },
+        logo: '',
+        genres: [],
+        outdoor: false,
+        sources: {
+            images: ''
+        }
 
     })
 
@@ -27,31 +43,44 @@ const NewFestivalForm = () => {
     const handleInputChange = (e) => {
         const { value, name } = e.target
         setNewFestival({ ...newFestival, [name]: value })
-
     }
 
     return (
 
-        <Container>
+        <Container className="NewFestivalForm">
             <Row>
-                <Col md={{ span: 6, offset: 2 }}>
+                <Col md={{ span: 10, offset: 1 }}>
                     <h3>New Festival</h3>
                     <hr />
                     <Form className="mt-5" onSubmit={handleFormSubmit}>
                         <Row>
                             <Col>
                                 <Form.Group className="mb-3" controlId="name">
-                                    <Form.Label>Fest Title </Form.Label>
+                                    <Form.Label>Festival Name</Form.Label>
                                     <Form.Control
                                         type="text"
                                         value={newFestival.name}
                                         onChange={handleInputChange}
                                         name={'name'}
                                     />
-                                    <Form.Text className="text-muted"></Form.Text>
+
                                 </Form.Group>
                             </Col>
 
+                            <Col>
+                                <Form.Group className="mb-3" controlId="shortDescription">
+                                    <Form.Label>Short Description</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        value={newFestival.shorDescription}
+                                        onChange={handleInputChange}
+                                        name={'shortDescription'}
+                                    />
+                                    <Form.Text className="text-muted">You will read this text in the Festival Card</Form.Text>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
                             <Col>
                                 <Form.Group className="mb-3" controlId="description">
                                     <Form.Label>Description</Form.Label>
@@ -64,13 +93,112 @@ const NewFestivalForm = () => {
                                 </Form.Group>
                             </Col>
                         </Row>
+                        <Row>
+                            <Col>
+                                <Row>
+                                    <Col>
+                                        <Form.Label>Location</Form.Label>
+                                    </Col>
+                                </Row>
+                                <InputGroup className="mb-3" controlId="location">
+                                    <Form.Control
+                                        placeholder="Country"
+                                        type="text"
+                                        value={newFestival.location.country}
+                                        onChange={handleInputChange}
+                                        name={'country'}
+                                    />
+                                    <Form.Control
+                                        placeholder="City"
+                                        type="text"
+                                        value={newFestival.location.city}
+                                        onChange={handleInputChange}
+                                        name={'city'}
+                                    />
+                                    <Form.Control
+                                        placeholder="ZipCode"
+                                        type="number"
+                                        value={newFestival.location.zipcode}
+                                        onChange={handleInputChange}
+                                        name={'zipcode'}
+                                    />
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3" controlId="logo">
+                                    <Form.Label>Logo</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        value={newFestival.logo}
+                                        onChange={handleInputChange}
+                                        name={'logo'}
+                                        placeholder="http://"
+                                    />
+                                    <Form.Text className="text-muted">Insert the URL of the logo in png format</Form.Text>
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-3" controlId="images">
+                                    <Form.Label>Image</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        value={newFestival.sources.images}
+                                        onChange={handleInputChange}
+                                        name={'images'}
+                                        placeholder="http://"
+                                    />
+                                    <Form.Text className="text-muted">Insert the URL of the image</Form.Text>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Group controlId="genres">
+                                    <Row>
+                                        <Col>
+                                            <Form.Label>Musical genres</Form.Label>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <Container className="genres-box">
+                                                {
+                                                    genres.map((genre) => {
+                                                        return (
+                                                            <Form.Check
+                                                                className="genreCheckbox"
+                                                                inline
+                                                                label={genre}
+                                                                name={genre}
+                                                                type="checkbox"
+                                                                id={genre}
+                                                                key={genre}
+                                                                value={newFestival.genres}
+                                                                onChange={handleInputChange}
+                                                            />
+                                                        )
+                                                    })
+                                                }
+                                            </Container>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            < Form.Text className="text-muted">Select at least 5 musical genres</Form.Text>
+                                        </Col>
+                                    </Row>
+
+
+                                </Form.Group>
+                            </Col>
+                        </Row>
                         <div className="d-grid">
                             <Button variant="dark" type="submit">Create Festival</Button>
                         </div>
                     </Form>
-
                 </Col>
-
             </Row>
 
         </Container>
