36338225b13d9225dc5741f6d5245b927294c644
pedro roca de togores <pedrobgrdt@gmail.com>
2024-02-22 15:49:14 +0100

    Hp css

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,10 +1,13 @@
-import { Carousel, Container } from 'react-bootstrap'
+import { Carousel, Container, Card, Row, Col, } from 'react-bootstrap'
 import './HomePage.css'
 import burningManHp from './../../assets/images/burning-man.jpg'
 import tommorrrowLandHp from './../../assets/images/tommorrowland-hp-image.jpg'
 import snowBombingHp from './../../assets/images/snowbombing-HP-image.webp'
 import ultraEuropeHp from './../../assets/images/ultraeu_HP_image.jpeg'
 import primaveraSoundHp from './../../assets/images/primaverasound_HP_image.jpg'
+import cardFestHp from './../../assets/images/cardimagehp.jpg'
+import cardEditHp from './../../assets/images/editioncardhp.jpg'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
 
@@ -12,40 +15,75 @@ const HomePage = () => {
     return (
 
         <div className='HomePage'>
-            <Container>
 
-                <Carousel>
-                    <Carousel.Item>
-                        <img src={burningManHp}
-                            alt='burningManImage'
+            {/* <div className='carouselContainer'>
+                <Container>
+                    <Carousel className='imgCarouselHp'>
+                        <Carousel.Item>
+                            <img src={burningManHp}
+                                alt='burningManImage'
 
-                        />
-                    </ Carousel.Item>
-                    <Carousel.Item>
-                        <img src={tommorrrowLandHp}
-                            alt='tommorrowLandImage'
-                        />
-                    </ Carousel.Item>
-                    <Carousel.Item>
-                        <img src={snowBombingHp}
-                            alt='snowBombingImage'
-                        />
-                    </ Carousel.Item>
-                    <Carousel.Item>
-                        <img src={ultraEuropeHp}
-                            alt='ultraEuImage'
-                        />
-                    </ Carousel.Item>
-                    <Carousel.Item>
-                        <img src={primaveraSoundHp}
-                            alt='primaveraSoundImage'
-                        />
-                    </ Carousel.Item>
-                </Carousel>
+                            />
+                        </ Carousel.Item>
+                        <Carousel.Item>
+                            <img src={tommorrrowLandHp}
+                                alt='tommorrowLandImage'
+                            />
+                        </ Carousel.Item>
+                        <Carousel.Item>
+                            <img src={snowBombingHp}
+                                alt='snowBombingImage'
+                            />
+                        </ Carousel.Item>
+                        <Carousel.Item>
+                            <img src={ultraEuropeHp}
+                                alt='ultraEuImage'
+                            />
+                        </ Carousel.Item>
+                        <Carousel.Item>
+                            <img src={primaveraSoundHp}
+                                alt='primaveraSoundImage'
+                            />
+                        </ Carousel.Item>
+                    </Carousel>
+                </Container>
+            </div> */}
+            <div className="editioncard-hp">
 
-            </Container>
+                <Row>
+                    <Col>
+                        <Link to={`/editions/`} className="EditionCardLinkHp">
+                            <Card className="EditiHp">
+                                <div className="imgContainerEditionHp">
+                                    <Card.Img variant="top" src={cardEditHp} />
+                                </div>
+                                <Card.Body className="infoContainerEditionHp">
+                                    <Card.Title><stron>Explore Editions</stron></Card.Title>
 
-        </div>
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+
+                        <Link to={`/festivals/`} className="FestivalCardLinkHp">
+                            <Card className="FestivalCardHp" >
+                                <div className="imgContainerFestivalCardHp">
+                                    <Card.Img variant="top" src={cardFestHp} />
+                                </div>
+                                <Card.Body className="infoContainerFestivalCardHp">
+                                    <Card.Title>Explore Festivals</Card.Title>
+
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </Col>
+                </Row>
+
+            </div>
+        </div >
 
 
     )
